7d4ce92782810916b1113b6a41826a32a064b7ba
Luke Stynes <lst89@uclive.ac.nz>
2024-11-06 15:03:22 +1300

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 // import SmoothScrolling from "@/components/smooth-scrolling";
 
 export const metadata = {
@@ -20,6 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="G-GP8NQ0L6TC" />
+      <head>
+        <Script
+          async
+          src="https://cloud.umami.is/script.js"
+          data-website-id="1e9e4307-f152-4192-a6d5-3eca49fc6fba"
+        />
+      </head>
       <body className={`${GeistSans.className}`}>
         <SpeedInsights />
         <Analytics />
